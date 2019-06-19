dfd5e29414f44ebfcd95ad88fad330b947ad0f67
Hamish Lees <hamishlees6@gmail.com>
2019-06-19 20:02:04 +1000

    Functioning, where you can add songs to the list and play them in order

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,56 +1,84 @@
-import React, { Component } from "react";
-import youtube from './../apis/youtube'
+/*
+Stretch:
+  1) Add in authorisation, so we can see who is uploading
+  2) Save the current video to local storage in case of refresh
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+  3) Get title:
+      document.getElementsByClassName('ytp-title-link yt-uix-sessionlink')[0].innerText
+*/
+
+import React, { Component } from "react";
+import ReactPlayer from 'react-player';
 
 class App extends Component {
   state = {
     videos: [],
-    selectedVideo: null
+    url: null,
+    value: "",
+    title: ""
   }
 
-  handleSubmit = async (termFromSearchBar) => {
-    const response = await youtube.get('/search', {
-        params: {
-            q: termFromSearchBar
-        }
-    })
+  // At the moment, we are just going to enter the youtube URL itself
+  // In the future, we will let people do a search
+  handleSubmit = async (event) => {
+    event.preventDefault();
+    if (!this.state.url) this.setState({url: this.state.value})
+    let tempArr = this.state.videos;
+    tempArr.push(this.state.value);
+    this.state.value = "";
     this.setState({
-        videos: response.data.items
-    })
+      videos: tempArr
+    });  
   };
-  
-  handleVideoSelect = (video) => {
-      this.setState({selectedVideo: video})
-  }
+
+  handleChange = (event) => this.setState({value: event.target.value});
+
+  endOfVideo = () => {
+    let tempArr = this.state.videos;
+    tempArr.shift();
+    this.setState({
+      videos: tempArr,
+      url: tempArr[0],
+      title: document.getElementsByClassName('ytp-title-link yt-uix-sessionlink')[0].innerText
+    });
+  };
+
 
   render(){
+    console.log(this.state.title)
     return(
-      <div>Welcome</div>
+      <div>
+        <div>
+          <div>
+            {this.state.videos.length > 0 ?  
+            <div>
+              <h4>Up Next..</h4>
+              <ul>
+                {this.state.videos.map((video) => (
+                  <li key={video}>{video}</li>
+                ))}
+              </ul>
+            </div> : <div><h4>No songs yet :( Add one?</h4></div>
+            }
+          </div>
+          <div>
+            <form onSubmit={this.handleSubmit}>
+              <input type="text" value={this.state.value} onChange={this.handleChange} />
+              <input type="submit" value="Add Video"/>
+            </form>
+          </div>
+          <div>
+            {this.state.url && 
+              <ReactPlayer 
+                url={this.state.url}  
+                controls
+                playing
+                onEnded={() => this.endOfVideo()}/>}
+          </div>
+        </div>
+      </div>
     )
   }
 }
-
-
-
 
 export default App;
